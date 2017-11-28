a91c1958ce0ee74f18b1f239bfe03d39cbd0ad66
Ginne Chou <cocacolabear.la@gmail.com>
2017-11-28 01:53:50 -0500

    fix: solution for question one and two

--- solutions.py
+++ solutions.py
@@ -1,30 +1,85 @@
 #!/usr/bin/env python
 
 
-""
-"
+"""
 Question 1
 Given two strings s and t, determine whether some anagram of t is a substring of s.For example: if s = "udacity"
 and t = "ad", then the
 function returns True.Your
 function definition should look like: question1(s, t) and
 return a boolean True or False.
-""
-"
+"""
 
-""
-"
+def questionOne(s, t):
+  l = len(t)
+  dictT = {}
+    for chr in t:
+      if chr not in dictT.keys():
+        dictT[chr] = 1
+      else:
+        dictT[chr] += 1
+        
+    for i in range(0, len(s)-l+1):
+      subStr = s[i:i+l]
+      subDic = {}
+        for chr not in dictT.keys():
+          break
+        else:
+          if chr not in subDic.keys():
+            subDic[chr] = 1
+          else:
+            subDic[chr] += 1
+      if subDic == dictT: return True
+     return False
+
+ print questionOne('','Empty')
+ print questionOne('aa','aaccccc')
+ print questionOne('Udacity','')
+
+"""
 
 Question 2
 Given a string a, find the longest palindromic substring contained in a.Your
 function definition should look like question2(a), and
 return a string.
 
-""
-"
+"""
 
-""
-"
+def questionTwo(a):
+
+	def palindrome(s, p1, p2):
+		l = p1
+		r = p2
+		n = len(s)
+		while ((r <= n-1) & (l >= 0)):
+			if (s[l] == s[r]):
+				l-=1
+				r+=1
+			else:
+				break
+		return s[l+1:r]
+
+	n = len(a)
+	if (n==0): return ""
+	longest = a[0]
+	for i in range(0, n-1):
+		pOne = palindrome(a, i, i)
+		if (len(pOne) > len(longest)):
+			longest = pOne
+		pTwo = palindrome(a, i, i+1)
+		if (len(pTwo) > len(longest)):
+			longest = pTwo
+	return longest
+
+a = 'fdafjkdkjfdvbn'
+
+print questionTwo('abccbadef')
+print questionTwo(' ')
+print questionTwo('USDHSBufdsufhsudhsckasduhurqohcbsaqo')
+
+
+"""
+
 Question 3
 Given an undirected graph G, find the minimum spanning tree within G.A minimum spanning tree connects all vertices in a graph with the smallest possible total weight of edges.Your
 function should take in and
@@ -38,11 +93,10 @@ return an adjacency list structured like this:
 
 Vertices are represented as unique strings.The
 function definition should be question3(G)
-""
-"
+"""
 
-""
-"
+
+"""
 
 Question 4
 Find the least common ancestor between two nodes on a binary search tree.The least common ancestor is the farthest node from the root that is an ancestor of both nodes.For example, the root is a common ancestor of all nodes on the tree, but
@@ -60,11 +114,11 @@ question4([
   4)
 and the answer would be 3.
 
-  ""
-"
+ """
 
-""
-"
+
+"""
+
 Question 5
 Find the element in a singly linked list that 's m elements from the end. For example, if a linked list has 5 elements, the 3rd element from the end is the 3rd element. The function definition should look like question5(ll, m), where ll is the first node of a linked list and m is the "mth number from the end". You should copy/paste the Node class below to use as a representation of a node in the linked list. Return the value of the node at that position.
 
@@ -73,5 +127,5 @@ class Node(object):
   self.data = data
 self.next = None
 
-  ""
-"
+ """
+
