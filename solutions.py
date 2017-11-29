81d4e01008f6d0ee3d0fff4eaa7b54eedb6a1533
Ginne Chou <cocacolabear.la@gmail.com>
2017-11-29 01:26:03 -0500

    fix: Add solution for question four

--- solutions.py
+++ solutions.py
@@ -176,6 +176,31 @@ and the answer would be 3.
 
  """
 
+def question4(T, r, n1, n2):
+    
+    space = []
+    while n1 != r:
+        n1 = parent(T, n1)
+        space.append(n1)
+    if len(space) == 0:
+        return -1
+    while n2 != r:
+        n2 = parent(T, n2)
+        if n2 in space:
+            return n2
+    return -1
+    
+def parent(T, n):
+	#return parent of n if it exists, otherwise return -1
+    numrows = len(T)
+    for i in range(numrows):
+        if T[i][n] == 1:
+            return i
+    return -1
+
+
+print question4([[0,1,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1],[0,0,0,0,0]],3,1,4)
+
 
 """
 
